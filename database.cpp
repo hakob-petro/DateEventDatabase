69a3a68d10fcb12ad990ec4b2e00f60d1b49fcf2
Hakob <petrosyanhakob-2000@mail.ru>
2021-08-17 14:11:00 +0400

    Optimized

--- database.cpp
+++ database.cpp
@@ -1,6 +1,5 @@
 #include "database.h"
 
-#include <algorithm>
 #include <stdexcept>
 #include <iostream>
 using namespace std;
@@ -13,34 +12,83 @@ ostream& operator << (ostream& stream, const Entry& de)
 	return stream;
 }
 
+bool operator < (const Entry& entry1, const Entry& entry2)
+{
+	return entry1.date_ < entry2.date_;
+}
+
+bool operator > (const Entry& entry1, const Entry& entry2)
+{
+	return entry1.date_ > entry2.date_;
+}
+
+bool operator == (const Entry& entry1, const Entry& entry2)
+{
+	return entry1.date_ == entry2.date_ &&
+		entry1.event_ == entry2.event_;
+}
+
 void Database::Add(const Date& date, const string& event)
 {
-	const auto it = find(base_[date].begin(), base_[date].end(), event);
-	if (it == base_[date].end())
+	if(check_base_[date].count(event) == 0)
 	{
-		base_[date].push_back(event);
+		base_.insert({date, event});
+		check_base_[date].insert(event);
 	}
 }
 
 ostream& Database::Print(ostream& stream) const
 {
-	for(const pair<const Date, vector<string>> &elem : base_)
+	for (const Entry& entry : base_)
 	{
-		for(const string &event : elem.second)
-		{
-			stream << elem.first << " " << event << endl;
-		}
+		stream << entry.date_ << " " << entry.event_ << endl;
 	}
+
 	return stream;
 }
 
 Entry Database::Last(const Date& date) const
 {
+	const Entry empty_entry = {date, ""};
 	// it points to the first Entry with date more then given 
-	auto it = base_.upper_bound(date);
-	
+	auto it = base_.upper_bound(empty_entry);
+
 	if (it == begin(base_)) throw invalid_argument("No events with date less then given!");
 	// --it points to the last Entry with date not less then given
 	--it;
-	return {it->first, it->second.back()}; // return the date and the last event	
+	return *it; // return the date and the last event
+}
+
+int Database::RemoveIf(const Predicate& predicate)
+{
+	int count = 0;
+	
+	for(auto it = base_.begin(); it != base_.end();)
+	{
+		if (predicate(it->date_, it->event_))
+		{
+			check_base_[it->date_].erase(it->event_);
+			it = base_.erase(it);
+			++count;
+		}
+		else
+		{
+			++it;
+		}
+	}
+	return count;
+}
+
+std::vector<Entry> Database::FindIf(const Predicate& predicate) const
+{
+	vector<Entry> found;
+	
+	for(const Entry &entry : base_)
+	{
+		if (predicate(entry.date_, entry.event_))
+		{
+			found.push_back(entry);
+		}
+	}
+	return found;
 }
--- database.h
+++ database.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "date.h"
 
+#include <set>
 #include <map>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <functional>
+
+using Predicate = std::function<bool(const Date& date, const std::string& event)>;
 
 struct Entry
 {
@@ -16,6 +19,13 @@ struct Entry
 
 std::ostream& operator << (std::ostream& stream, const Entry& de);
 
+bool operator < (const Entry& entry1, const Entry& entry2);
+
+bool operator == (const Entry& entry1, const Entry& entry2);
+
+bool operator > (const Entry& entry1, const Entry& entry2);
+
+
 class Database
 {
 public:
@@ -25,78 +35,11 @@ public:
 	
 	Entry Last(const Date& date) const;
 
-	// declaration of the template function RemoveIf wich will get a lambda expression prediacte (function<bool(Date, string)> from
-	// library <functional>) and remove all Entrys for what predicate returns true
-	// returns the count of deleted events
-	template<typename T>
-	int RemoveIf(T predicate)
-	{
-		int count = 0;
-		auto map_iter = begin(base_);
-		while (map_iter != end(base_))
-		{
-			const Date& date = map_iter->first;
+	int RemoveIf(const Predicate& predicate);
 
-			// definition of the new lambda expresion lambda (function<bool(Date, string)> from library <functional>)
-			// which does that the same as the predicate, but for certain Date
-			auto lambda = [date, predicate](const std::string& event)->bool { return !predicate(date, event); };
-
-			// partition will save the order of events wich won't be deleted
-			std::vector<std::string>& vector_events = map_iter->second;
-			auto iter = stable_partition(begin(vector_events), end(vector_events), lambda);
-
-			// all events that must be removed are stored in the end of vector,
-			// so we count the number of that events and efficiently delete them  from the end of vector(algorithmic complexity - O(n),
-			// by comparison std::vector.erase with algorithmic complexity - O(n^2))
-			int i = end(vector_events) - iter;
-			while (i > 0)
-			{
-				vector_events.pop_back();
-				++count;
-				--i;
-			}
-
-			// check if there are no events, delete the date
-			if (vector_events.empty())
-				map_iter = base_.erase(map_iter);
-			else
-				++map_iter;
-		}
-		return count;
-	}
-
-	// declaration of the template function FindIf wich will get a lambda expression prediacte (function<bool(Date, string)> from
-	// library <functional>) and put into vector all Entrys for what predicate returns true
-	// returns a vector that events
-	template<typename T>
-	std::vector<Entry> FindIf(const T predicate) const
-	{
-		std::vector<Entry> result;
-		auto map_iter = begin(base_);
-		while (map_iter != end(base_))
-		{
-			const Date& date = map_iter->first;
-
-			// definition of the new lambda expresion lambda (function<bool(Date, string)> from library <functional>)
-			// which does that the same as the predicate, but for certain Date
-			auto lambda = [date, predicate](const std::string& event)->bool{ return predicate(date, event); };
-
-			const std::vector<std::string>& vector_events = map_iter->second;
-
-			auto iter = begin(vector_events);
-			while (iter != end(vector_events)) {
-				iter = find_if(iter, end(vector_events), lambda);
-				if (iter != end(vector_events)) {
-					result.push_back({ date, *iter });
-					++iter;
-				}
-			}
-
-			++map_iter;
-		}
-		return result;
-	}
+	std::vector<Entry> FindIf(const Predicate& predicate) const;
 
 private:
-	std::map<Date, std::vector<std::string>> base_;
+	std::multiset<Entry, std::less<>> base_;
+	std::map<Date, std::set<std::string>> check_base_;
 };
